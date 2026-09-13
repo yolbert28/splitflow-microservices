911acbb3a58ad007b7dbd09378ddb85889f2dbde
yolbert28 <yolbertdev@gmail.com>
2026-09-13 16:09:38 -0400

    first commit

---
+++ auth_service/src/test/java/dev/yolbert/auth_service/TestAuthServiceApplication.java
@@ -0,0 +1,11 @@
+package dev.yolbert.auth_service;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(AuthServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
---
+++ auth_service/src/test/java/dev/yolbert/auth_service/TestcontainersConfiguration.java
@@ -0,0 +1,25 @@
+package dev.yolbert.auth_service;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.postgresql.PostgreSQLContainer;
+import org.testcontainers.rabbitmq.RabbitMQContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestcontainersConfiguration {
+
+	@Bean
+	@ServiceConnection
+	PostgreSQLContainer postgresContainer() {
+		return new PostgreSQLContainer(DockerImageName.parse("postgres:latest"));
+	}
+
+	@Bean
+	@ServiceConnection
+	RabbitMQContainer rabbitContainer() {
+		return new RabbitMQContainer(DockerImageName.parse("rabbitmq:latest"));
+	}
+
+}
